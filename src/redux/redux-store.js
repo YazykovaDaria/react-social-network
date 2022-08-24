cb1a0e654dd72a71156761fcf687fcc84553518e
YazykovaDaria <yazykovadasva@gmail.com>
2022-08-24 10:57:41 +0300

    add redux extension

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import {legacy_createStore as createStore, combineReducers, applyMiddleware} from 'redux'
+import {legacy_createStore as createStore, combineReducers, applyMiddleware, compose} from 'redux'
 import { profileReducer } from './profileReducer';
 import dialogReducer from './dialogReducer';
 import usersReducer from './usersReducer';
@@ -16,6 +16,9 @@ const reducers = combineReducers({
   app: appReducer
 })
 
-const store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)))
+//const store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
 export default store;
